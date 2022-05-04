68932196791b6be207cd8d01bb3bcbedeed684cb
Tonnie <vontonnie@gmail.com>
2022-05-04 21:23:48 +0300

    Insert API Key

--- app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
-
+const val VANTAGE_API_KEY = "EWFVFPOUVDKBT16Q"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

4347b6203600c9a33056cae348ae9ccedf237fdc
Tonnie <vontonnie@gmail.com>
2022-05-05 08:44:13 +0300

    Add Base URL

--- app/src/main/java/com/uxstate/stockmarketapp/data/remote/StockAPI.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/data/remote/StockAPI.kt
@@ -1,6 +1,18 @@
 package com.uxstate.stockmarketapp.data.remote
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface StockAPI {
 
-    
+
+    @GET()
+    suspend fun getListings( @Query("apikey") apiKey:String)
+
+    companion object {
+
+        const val API_KEY = "EWFVFPOUVDKBT16Q"
+        const val BASE_URL = "https:/alphavantage.co"
+    }
+
 }

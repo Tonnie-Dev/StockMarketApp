3cd01f03ab4c122e8616ce03b61b48e3cf1f5d46
Tonnie <vontonnie@gmail.com>
2022-05-09 07:37:50 +0300

    Add toCompanyListingEntity ext fxn in the mapper file

--- app/src/main/java/com/uxstate/stockmarketapp/data/mapper/CompanyMapper.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -3,10 +3,28 @@ package com.uxstate.stockmarketapp.data.mapper
 import com.uxstate.stockmarketapp.data.local.CompanyListingEntity
 import com.uxstate.stockmarketapp.domain.model.CompanyListing
 
-fun CompanyListingEntity.toCompanyListing():CompanyListing{
+
+//map entity to model
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
 
 
-    return CompanyListing(symbol = this.symbol, name = this.name, exchange = this.exchange)
+    return CompanyListing(
+        symbol = this.symbol,
+        name = this.name,
+        exchange = this.exchange
+    )
 
 
+}
+
+
+//map from model to entity
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+
+    return CompanyListingEntity(
+        symbol = this.symbol,
+        name = this.name,
+        exchange = this.exchange,
+        id = null
+    )
 }

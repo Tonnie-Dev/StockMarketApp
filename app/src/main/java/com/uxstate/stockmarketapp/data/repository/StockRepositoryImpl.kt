9ab52d2f93ee6c32f5c8421bf7234458c694608a
Tonnie <vontonnie@gmail.com>
2022-05-15 20:31:41 +0300

    Add Stock Repository class

--- app/src/main/java/com/uxstate/stockmarketapp/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/data/repository/StockRepositoryImpl.kt
@@ -1,9 +1,62 @@
 package com.uxstate.stockmarketapp.data.repository
 
+import com.uxstate.stockmarketapp.data.local.CompanyListingDatabase
+import com.uxstate.stockmarketapp.data.mapper.toCompanyListing
+import com.uxstate.stockmarketapp.data.remote.StockAPI
+import com.uxstate.stockmarketapp.domain.model.CompanyListing
+import com.uxstate.stockmarketapp.domain.repository.StockRepository
+import com.uxstate.stockmarketapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 import javax.inject.Singleton
 
 
-//force single instance with @Singleton
+//force single instance of our repository for the entire app
 @Singleton
-class StockRepositoryImpl {
+class StockRepositoryImpl
+@Inject constructor(
+    val api: StockAPI,
+    val db: CompanyListingDatabase
+) : StockRepository {
+
+    private val dao = db.dao
+
+    override suspend fun getCompanyListings(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> {
+
+        //return a flow builder
+        return flow {
+
+//inside a flow builder we have access to emit
+
+            //ABOUT TO START DATABASE QUERY
+
+            //emit a loading resource
+            emit(Resource.Loading(isLoading = true))
+
+
+            //QUERYING DB
+            //obtain the list of companies from db
+            val localListing = dao.searchCompanyListings(query = query)
+
+            /*at this point we have successfully loaded the cache, if
+            * the database is empty then localListing will be an
+            * empty list*/
+
+            // emit a success resource which is ready to be shown in the UI
+            emit(Resource.Success(data = localListing.map { it.toCompanyListing() }))
+
+
+
+            /* check if we actually need at the API Call - if we don't
+            * swip to refresh the we have up to date listings and we
+            * don't need an API call*/
+
+            //
+
+        }
+    }
 }

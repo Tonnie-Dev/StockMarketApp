83fe214b7defedc7207a6f2c19d1aa953bf69f3c
Tonnie <vontonnie@gmail.com>
2022-05-17 19:20:38 +0300

    Add getCompanyListings() viewModel function

--- app/src/main/java/com/uxstate/stockmarketapp/presentation/company_listings/CompanyListingsViewModel.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/company_listings/CompanyListingsViewModel.kt
@@ -4,15 +4,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.stockmarketapp.domain.repository.StockRepository
+import com.uxstate.stockmarketapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
-class CompanyListingsViewModel @Inject constructor(repository: StockRepository) : ViewModel() {
+class CompanyListingsViewModel @Inject constructor(val repository: StockRepository) : ViewModel() {
 
     var state by mutableStateOf(CompanyListingsState())
         private set
@@ -21,15 +24,55 @@ class CompanyListingsViewModel @Inject constructor(repository: StockRepository) 
     val uiEvent = _uiEvent.receiveAsFlow()
 
 
-
-    fun onEvent(event: CompanyListingsEvent){
-
-
-        when (event){
+    fun onEvent(event: CompanyListingsEvent) {
 
 
-            is CompanyListingsEvent.Refresh -> {}
+        when (event) {
+
+
+            is CompanyListingsEvent.Refresh -> {
+
+                getCompanyListings(fetchFromRemote = true)
+            }
             is CompanyListingsEvent.OnSearchQueryChange -> {}
         }
+    }
+
+    //Called on Swipe to Refresh data
+    fun getCompanyListings(
+        query: String = state.searchQuery,
+        fetchFromRemote: Boolean = false
+    ) {
+
+        //launch Coroutine
+
+        viewModelScope.launch {
+
+
+            repository.getCompanyListings(
+                query = query,
+                fetchFromRemote = fetchFromRemote
+            )
+                    .collect {
+
+                            result ->
+                        when (result) {
+
+                            is Resource.Loading -> {
+
+                                state = state.copy(isLoading = result.isLoading)
+                            }
+                            is Resource.Success -> {
+
+                              result.data?.let {
+                                  state = state.copy(companies = it)
+                              }
+                            }
+                            is Resource.Error -> Unit
+                        }
+                    }
+        }
+
+
     }
 }

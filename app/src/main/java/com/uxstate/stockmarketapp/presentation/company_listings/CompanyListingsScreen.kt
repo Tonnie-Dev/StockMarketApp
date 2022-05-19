547a11711090da505c37ea1c84f228166b9cfa92
Tonnie <vontonnie@gmail.com>
2022-05-19 08:09:23 +0300

    Add SwipeRefresh Composable to CompanyListingsScreen

--- app/src/main/java/com/uxstate/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -23,24 +25,47 @@ fun CompanyListingsScreen(
 ) {
 
     val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = viewModel.state.isRefreshing)
-    val state = viewModel.state
+    val state =viewModel.state
 
     Column(modifier = Modifier.fillMaxSize()) {
 
         OutlinedTextField(
                 value = viewModel.state.searchQuery,
-                onValueChange = { viewModel.onEvent(CompanyListingsEvent.OnSearchQueryChange(it)) },
+                onValueChange = {
+
+                    //send an event to ViewModel
+                    viewModel.onEvent(CompanyListingsEvent.OnSearchQueryChange(it)) },
                 modifier = Modifier
                         .fillMaxWidth()
                         .padding(16.dp),
                 placeholder = { Text(text = "Search...")},
                 maxLines = 1,
                 singleLine = true,
-                
-                
                 )
 
 
+
+        SwipeRefresh(state = swipeRefreshState,
+
+                //send an event to the ViewModel
+                onRefresh = {
+
+                    viewModel.onEvent(CompanyListingsEvent.Refresh)
+                }) {
+
+
+            LazyColumn(content = {
+
+
+                items(state.companies.size){
+
+                    if (state.companies.size >)
+                }
+            })
+
+
+
+        }
     }
 
 }

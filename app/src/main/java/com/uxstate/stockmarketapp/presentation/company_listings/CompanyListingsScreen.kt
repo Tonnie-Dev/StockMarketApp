7f76a79ee26624af28960cde9adadbb2144fc15d
Tonnie <vontonnie@gmail.com>
2022-05-19 08:33:33 +0300

    Add Lazy Column to SwipeRefresh

--- app/src/main/java/com/uxstate/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -1,10 +1,12 @@
 package com.uxstate.stockmarketapp.presentation.company_listings
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,7 +27,7 @@ fun CompanyListingsScreen(
 ) {
 
     val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = viewModel.state.isRefreshing)
-    val state =viewModel.state
+    val state = viewModel.state
 
     Column(modifier = Modifier.fillMaxSize()) {
 
@@ -34,14 +36,15 @@ fun CompanyListingsScreen(
                 onValueChange = {
 
                     //send an event to ViewModel
-                    viewModel.onEvent(CompanyListingsEvent.OnSearchQueryChange(it)) },
+                    viewModel.onEvent(CompanyListingsEvent.OnSearchQueryChange(it))
+                },
                 modifier = Modifier
                         .fillMaxWidth()
                         .padding(16.dp),
-                placeholder = { Text(text = "Search...")},
+                placeholder = { Text(text = "Search...") },
                 maxLines = 1,
                 singleLine = true,
-                )
+        )
 
 
 
@@ -57,12 +60,22 @@ fun CompanyListingsScreen(
             LazyColumn(content = {
 
 
-                items(state.companies.size){
+                items(state.companies.size) { i ->
+                    val company = state.companies[i]
+                    CompanyItem(company = company, modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
 
-                    if (state.companies.size >)
+                                // TODO: Navigate to Detail Screen
+                            }.padding(16.dp))
+
+                    //if not at the last item we show a divider
+                    if (i < state.companies.size) {
+
+                        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+                    }
                 }
             })
-
 
 
         }

ac2ef87495e642c8cd9a2c714d6e1d572255f976
Tonnie <vontonnie@gmail.com>
2022-05-20 21:05:53 +0300

    Add internet permissions in the manifest

--- app/src/main/java/com/uxstate/stockmarketapp/StockMarketApplication.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/StockMarketApplication.kt
@@ -3,7 +3,7 @@ package com.uxstate.stockmarketapp
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+@HiltAndroidApp //Tells Hilt this is our application class
 class StockMarketApplication:Application() {
 
 

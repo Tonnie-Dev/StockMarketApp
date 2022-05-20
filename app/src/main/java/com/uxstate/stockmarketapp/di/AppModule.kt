83677bdab664daaa1612b3de77e91369801964ab
Tonnie <vontonnie@gmail.com>
2022-05-20 08:51:18 +0300

    Add provideStockAPI() AppModule function

--- app/src/main/java/com/uxstate/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/di/AppModule.kt
@@ -12,6 +12,9 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -36,5 +39,16 @@ object AppModule {
     ): StockRepository {
 
         return StockRepositoryImpl(api = api, db = db, companyListingParser = parser)
+    }
+
+    //provide API
+
+    fun provideStockAPI(): StockAPI {
+
+        return Retrofit.Builder()
+                .baseUrl(StockAPI.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create()
     }
 }

754ec728b4ccf8f2a859c74d5b000118eeefb6f1
Tonnie <vontonnie@gmail.com>
2022-05-20 08:40:43 +0300

    Add provideDatabase () App Module function

--- app/src/main/java/com/uxstate/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/di/AppModule.kt
@@ -1,14 +1,24 @@
 package com.uxstate.stockmarketapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.uxstate.stockmarketapp.data.local.CompanyListingDatabase
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 
 object AppModule {
 
-    
+    //provide Database
+
+    fun provideDatabase(@ApplicationContext context: Context): CompanyListingDatabase {
+
+
+        return Room.databaseBuilder(context, CompanyListingDatabase::class.java, "Company Listing")
+                .build()
+    }
 }

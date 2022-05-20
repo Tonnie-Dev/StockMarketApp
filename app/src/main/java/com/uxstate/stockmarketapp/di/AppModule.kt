ff6eb13fd10acba42aeeac3bc511c9231f9a1755
Tonnie <vontonnie@gmail.com>
2022-05-20 08:45:59 +0300

    Add provideStockRepository() AppModule function

--- app/src/main/java/com/uxstate/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/di/AppModule.kt
@@ -2,7 +2,12 @@ package com.uxstate.stockmarketapp.di
 
 import android.content.Context
 import androidx.room.Room
+import com.uxstate.stockmarketapp.data.csv.CVSParser
 import com.uxstate.stockmarketapp.data.local.CompanyListingDatabase
+import com.uxstate.stockmarketapp.data.remote.StockAPI
+import com.uxstate.stockmarketapp.data.repository.StockRepositoryImpl
+import com.uxstate.stockmarketapp.domain.model.CompanyListing
+import com.uxstate.stockmarketapp.domain.repository.StockRepository
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -20,5 +25,16 @@ object AppModule {
 
         return Room.databaseBuilder(context, CompanyListingDatabase::class.java, "Company Listing")
                 .build()
+    }
+
+    //provider Repository
+
+    fun provideStockRepository(
+        api: StockAPI,
+        db: CompanyListingDatabase,
+        parser: CVSParser<CompanyListing>
+    ): StockRepository {
+
+        return StockRepositoryImpl(api = api, db = db, companyListingParser = parser)
     }
 }

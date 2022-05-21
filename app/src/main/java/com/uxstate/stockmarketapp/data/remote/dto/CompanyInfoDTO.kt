df3fd40fbf2eea682072efebae8845e03c6f1ca0
Tonnie <vontonnie@gmail.com>
2022-05-21 16:45:13 +0300

    Add CompanyInfoDTO

---
+++ app/src/main/java/com/uxstate/stockmarketapp/data/remote/dto/CompanyInfoDTO.kt
@@ -0,0 +1,21 @@
+package com.uxstate.stockmarketapp.data.remote.dto
+
+import com.squareup.moshi.Json
+
+data class CompanyInfoDTO(
+
+    @field:Json(name = "Name")
+    val name: String,
+
+    @field:Json(name = "Symbol")
+    val symbol: String,
+
+    @field:Json(name = "Description")
+    val description: String,
+
+    @field:Json(name = "Country")
+    val country: String,
+
+    @field:Json(name = "Industry")
+    val industry: String
+)

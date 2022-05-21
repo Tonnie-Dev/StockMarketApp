24319ffb539cae01fd19c2dec50296f0340dcb62
Tonnie <vontonnie@gmail.com>
2022-05-21 13:21:15 +0300

    Change Theme

--- app/src/main/java/com/uxstate/stockmarketapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/ui/theme/Theme.kt
@@ -3,13 +3,20 @@ package com.uxstate.stockmarketapp.presentation.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+/*private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
+)*/
+
+private val DarkColorPalette = darkColors(
+        primary = Color.Green,
+        background = DarkBlue,
+        onPrimary = Color.DarkGray,
+        onBackground = TextWhite
 )
 
 /*private val LightColorPalette = lightColors(
@@ -17,31 +24,33 @@ private val DarkColorPalette = darkColors(
     primaryVariant = Purple700,
     secondary = Teal200*/
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+/* Other default colors to override
+background = Color.White,
+surface = Color.White,
+onPrimary = Color.White,
+onSecondary = Color.Black,
+onBackground = Color.Black,
+onSurface = Color.Black,
+*/
+
 
 @Composable
 fun StockMarketAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
+
+    //skip light/dark theme check
+   /* val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
-    }
+    }*/
 
     MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
+            colors = DarkColorPalette,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
     )
 }

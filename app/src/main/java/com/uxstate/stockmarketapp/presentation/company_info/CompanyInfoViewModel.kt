0caff21151ed0d28ba5b557da217468cd77b7958
Tonnie <vontonnie@gmail.com>
2022-05-22 17:39:10 +0300

    Add companyInfoViewModel

---
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/company_info/CompanyInfoViewModel.kt
@@ -0,0 +1,9 @@
+package com.uxstate.stockmarketapp.presentation.company_info
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CompanyInfoViewModel @Inject constructor() : ViewModel() {
+}

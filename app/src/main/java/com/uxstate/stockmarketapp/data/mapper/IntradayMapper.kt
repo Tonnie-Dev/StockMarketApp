414f8642dbfe881ea4c273daf265be5eda4c34b3
Tonnie <vontonnie@gmail.com>
2022-05-22 09:00:01 +0300

    Make CompanyInfoDTO nullable

--- app/src/main/java/com/uxstate/stockmarketapp/data/mapper/IntradayMapper.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/data/mapper/IntradayMapper.kt
@@ -1,8 +1,9 @@
 package com.uxstate.stockmarketapp.data.mapper
 
+import com.uxstate.stockmarketapp.data.remote.dto.CompanyInfoDTO
 import com.uxstate.stockmarketapp.data.remote.dto.IntradayInfoDTO
+import com.uxstate.stockmarketapp.domain.model.CompanyInfo
 import com.uxstate.stockmarketapp.domain.model.IntradayInfo
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -10,13 +11,17 @@ import java.util.*
 fun IntradayInfoDTO.toIntradayInfo(): IntradayInfo {
 
     val pattern = "yyyy-MM-dd HH:mm:ss"
-   val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
-   val localDateTime = LocalDateTime.parse(this.timeStamp, formatter)
-    return IntradayInfo(date = localDateTime,close = this.close)
+    val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
+    val localDateTime = LocalDateTime.parse(this.timeStamp, formatter)
+    return IntradayInfo(date = localDateTime, close = this.close)
 }
 
 
-fun IntradayInfo.toIntradayDTO():IntradayInfoDTO{
+fun CompanyInfoDTO.toCompanyInfo(): CompanyInfo {
+    return CompanyInfo(name = "", symbol = "", description = "", country = "", industry = "")
+}
+
+fun IntradayInfo.toIntradayDTO(): IntradayInfoDTO {
 
     return IntradayInfoDTO(timeStamp = this.date.toString(), close = this.close)
 }
--- app/src/main/java/com/uxstate/stockmarketapp/data/remote/dto/CompanyInfoDTO.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/data/remote/dto/CompanyInfoDTO.kt
@@ -2,20 +2,21 @@ package com.uxstate.stockmarketapp.data.remote.dto
 
 import com.squareup.moshi.Json
 
+/*Make fields nullable in case we hit allowed requests*/
 data class CompanyInfoDTO(
 
     @field:Json(name = "Name")
-    val name: String,
+    val name: String?,
 
     @field:Json(name = "Symbol")
-    val symbol: String,
+    val symbol: String?,
 
     @field:Json(name = "Description")
-    val description: String,
+    val description: String?,
 
     @field:Json(name = "Country")
-    val country: String,
+    val country: String?,
 
     @field:Json(name = "Industry")
-    val industry: String
+    val industry: String?
 )

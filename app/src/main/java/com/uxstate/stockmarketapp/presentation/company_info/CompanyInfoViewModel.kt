641608cf941b830e299b3e3d7eb85d3a5258c981
Tonnie <vontonnie@gmail.com>
2022-05-23 07:47:32 +0300

    Initialize CompanyInfoState in the ViewModel

--- app/src/main/java/com/uxstate/stockmarketapp/presentation/company_info/CompanyInfoViewModel.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/company_info/CompanyInfoViewModel.kt
@@ -1,5 +1,8 @@
 package com.uxstate.stockmarketapp.presentation.company_info
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.uxstate.stockmarketapp.domain.repository.StockRepository
@@ -11,4 +14,7 @@ class CompanyInfoViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
     private val repository: StockRepository
 ) : ViewModel() {
+
+    //initialize Company Info State
+    var state by mutableStateOf(CompanyInfoState())
 }

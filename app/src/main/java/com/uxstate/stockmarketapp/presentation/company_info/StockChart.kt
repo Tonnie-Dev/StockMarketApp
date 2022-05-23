4b011610c1296d6a86d50e837fddb13181d353f9
Tonnie <vontonnie@gmail.com>
2022-05-23 20:33:00 +0300

    Calculate the upper value for y-axis

--- app/src/main/java/com/uxstate/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/company_info/StockChart.kt
@@ -5,14 +5,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.uxstate.stockmarketapp.domain.model.IntradayInfo
+import kotlin.math.roundToInt
 
 @Composable
 fun StockChart(
-    info: List<IntradayInfo>,
+    infos: List<IntradayInfo>,
     modifier: Modifier = Modifier,
     graphColor: Color = Color.Green
 ) {
 
     val spacing = 100f
+
+    //cache values with remember so that they are not re-calculated
     var transparentGraphColor = remember { graphColor.copy(alpha = .5f) }
+
+    var upperValue = remember {
+        //find the highest value from infos list
+
+        //returns max of the close value or null
+        (infos.maxOfOrNull { it.close }
+                //add one as integer are rounded to floor
+                ?.plus(1))?.roundToInt() ?: 0
+    }
 }

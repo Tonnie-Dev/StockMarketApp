5417eb7a58c5f452c5b8aac54f742e965cd37d85
Tonnie <vontonnie@gmail.com>
2022-05-23 19:09:41 +0300

    Map state to repository.getCompanyInfo result

--- app/src/main/java/com/uxstate/stockmarketapp/presentation/company_info/CompanyInfoViewModel.kt
+++ app/src/main/java/com/uxstate/stockmarketapp/presentation/company_info/CompanyInfoViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.uxstate.stockmarketapp.domain.repository.StockRepository
+import com.uxstate.stockmarketapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,9 +27,32 @@ class CompanyInfoViewModel @Inject constructor(
         viewModelScope.launch {
 
             //get symbol parameter from SavedStateHandle
-
             val symbol = savedStateHandle.get<String>("symbol") ?: return@launch
+            //set the state to loading
 
+            state = state.copy(isLoading = true)
+
+            //make api calls - 2 network calls
+            /*we use async to run the coroutines independent of each other instead of
+            running the Coroutines sequentially*/
+
+            //use async coroutine builder to return a result of Deferred<T>
+            val companyInfoResult = async { repository.getCompanyInfo(symbol) }
+            val intradayInfoResult = async { repository.getIntradayInfo(symbol) }
+
+            when (val result = companyInfoResult.await()) {
+
+                is Resource.Success -> {
+
+                    state = state.copy(companyInfo = result.data, isLoading = false, error = null)
+                }
+                is Resource.Error -> {
+
+                    state =
+                        state.copy(error = result.message, isLoading = false, companyInfo = null)
+                }
+                else -> Unit
+            }
 
         }
     }
